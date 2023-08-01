5c3b3f016e54060eda573bb6806439d267b4cb74
Mikeyjpk <michaeljpkerr@gmail.com>
2023-08-01 16:37:48 +1000

    search filter

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
 
 const RootLayout = ({
     children
@@ -8,6 +9,10 @@ const RootLayout = ({
     return (
         <div className="h-full">
             <Navbar />
+            <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+                <Sidebar
+                 />
+            </div>
             <main className="md:pl-20 pt-16 h-full">
                 {children}
             </main>
